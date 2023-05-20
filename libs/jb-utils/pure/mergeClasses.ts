c7853113989b45fd95f5de92695872aa1a786b70
Han Han <28822945+hanhan9449@users.noreply.github.com>
2023-05-21 00:45:27 +0800

    初始化组件库工程

--- libs/jb-utils/pure/mergeClasses.ts
+++ libs/jb-utils/pure/mergeClasses.ts
@@ -1,3 +1,16 @@
-export function mergeClasses(...ss: string[]) {
-    return ss.join(' ')
+
+
+export function mergeClasses(...ss: string[]);
+export function mergeClasses() {
+    let result = ''
+    const n = arguments.length
+    for (let i = 0; i < n - 1; i++) {
+        if (arguments) {
+            result += arguments + ' '
+        }
+    }
+    if (n) {
+        result += arguments[n - 1]
+    }
+    return result
 }
