9dedd90f199bcc027badcc0ab820766c4618413e
hanhan9449 <28822945+hanhan9449@users.noreply.github.com>
2023-06-05 22:41:26 +0800

    feat: 新增app

---
+++ apps/blog-power-by-react/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>hello, this is ${"{"}blog-power-by-react{"}"}</div>
+    </>
+  )
+}
+
+export default App
