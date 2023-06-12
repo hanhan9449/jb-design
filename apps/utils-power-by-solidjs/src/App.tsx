97b59e452dfa5241badad2fc27eabe5ef43d366b
hanhan <28822945+hanhan9449@users.noreply.github.com>
2023-06-12 23:50:43 +0800

    feat: 样式修改；支持复制图片

--- apps/utils-power-by-solidjs/src/App.tsx
+++ apps/utils-power-by-solidjs/src/App.tsx
@@ -1,52 +1,74 @@
-import type { Component } from 'solid-js';
-import {QrCode} from "./components/QrCode";
-import {createSignal, onMount} from "solid-js";
-import copy from 'copy-to-clipboard'
-import {mergeClasses} from '@jb/utils'
-import {atom} from "./atom-css";
-import {JBButton, JBTextarea} from "@jb/solidjs-ui";
-import '@jb/solidjs-ui/style.css'
-
+import type { Component } from "solid-js";
+import { QrCode } from "./components/QrCode";
+import { createSignal, onMount } from "solid-js";
+import copy from "copy-to-clipboard";
+import { mergeClasses } from "@jb/utils";
+import { atom } from "./atom-css";
+import { JBButton, JBTextarea } from "@jb/solidjs-ui";
+import "@jb/solidjs-ui/style.css";
+import {toCanvas, toDataURL} from "qrcode";
 
 const App: Component = () => {
-    const [input, setInput] = createSignal('')
-    onMount(() => {
-        const u = new URL(location.href)
-        let share = u.searchParams.get('share');
-        console.log('share', share)
-        if (share) {
-            setInput(share)
-        }
-    })
+  const [input, setInput] = createSignal("");
+  onMount(() => {
+    const u = new URL(location.href);
+    let share = u.searchParams.get("share");
+    console.log("share", share);
+    if (share) {
+      setInput(share);
+    }
+  });
+  let canvasEl = document.createElement('canvas')
   return (
     <div>
-        <h2>二维码生成</h2>
-        <div>
-
-
+      <h2>二维码生成</h2>
+      <div>
         <JBTextarea
-            class={mergeClasses(atom.width500px, atom.fontSize20px, atom.fontFamilySanSerif)}
-            value={input()} onInput={(e) => {
-            setInput(e.target.value)
-        }}/>
-        </div>
-        <div>
-            <JBButton onClick={() => {
-                copy(input())
-                alert('复制成功')
-            }}>Copy</JBButton>
-            <JBButton onClick={() => {
-                const u = new URL(location.href)
-                u.searchParams.set('share', input())
-                copy(String(u))
-            }}>Share link</JBButton>
-        </div>
-        <div>
-
-
-      <QrCode value={input()}/>
-        </div>
-
+          class={mergeClasses(
+            atom.width500px,
+            atom.fontSize20px,
+            atom.fontFamilySanSerif
+          )}
+          value={input()}
+          onInput={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <JBButton
+          onClick={() => {
+            copy(input());
+          }}
+        >
+          Copy Text
+        </JBButton>
+          <JBButton onClick={async () => {
+              canvasEl.toBlob(blob => {
+                  if (blob) {
+                      navigator.clipboard.write([
+                          new ClipboardItem({
+                              "image/png": blob
+                          })
+                      ])
+                  }
+              })
+          }}>
+              Copy Image
+          </JBButton>
+        <JBButton
+          onClick={() => {
+            const u = new URL(location.href);
+            u.searchParams.set("share", input());
+            copy(String(u));
+          }}
+        >
+          Share Config link
+        </JBButton>
+      </div>
+      <div>
+        <QrCode value={input()} canvas={canvasEl} />
+      </div>
     </div>
   );
 };
--- apps/utils-power-by-solidjs/src/components/QrCode.tsx
+++ apps/utils-power-by-solidjs/src/components/QrCode.tsx
@@ -4,33 +4,55 @@ import localStyle from './QrCode.module.pcss'
 
 interface QrCodeProps {
     value?: string | null
+    canvas?: HTMLCanvasElement
 }
 
 export function QrCode(p: QrCodeProps) {
 
     const WIDTH = 300
-    const canvasEl = document.createElement('canvas')
+    const canvasEl = p.canvas || document.createElement('canvas')
+    const maskEl = document.createElement('canvas')
+    canvasEl.height = 300
+    canvasEl.width = 300
+
     const [qrDataUrl, {refetch}] = createResource(() => p.value, async () => {
         console.log('createResource', p.value)
         if (typeof p.value !== "string" || p.value.length === 0) {
             return ''
         }
-        return await toDataURL(canvasEl, p.value, {
+        // return ''
+        const dataUrl =  await toDataURL(maskEl, p.value, {
             width: WIDTH,
             margin: 0,
             color: {
-                light: '#00000000'
-            }
+                light: '#0000',
+                dark: ''
+            },
+            type: 'image/png'
         })
+        const ctx = canvasEl.getContext('2d')
+        if (!ctx) {
+            return
+        }
+        const gradient = ctx.createLinearGradient(0, 0, WIDTH, WIDTH)
+        gradient.addColorStop(0.1, 'peru')
+        gradient.addColorStop(0.3, 'pink')
+        gradient.addColorStop(0.7, 'tan')
+        gradient.addColorStop(0.9, 'lightseagreen')
+        ctx.fillStyle = gradient
+        ctx.globalCompositeOperation = 'source-over'
+        ctx.fillRect(0, 0, 300, 300)
+        ctx.globalCompositeOperation = "destination-in"
+        ctx.drawImage(maskEl, 0, 0, 300, 300)
+        return dataUrl
     })
     createEffect(() => {
         const value = p.value
         untrack(() => {
-            console.log('value', value)
             refetch(value)
         })
     })
     return <Show when={p.value?.length || 0 > 0}>
-        <div class={localStyle.qrcode} style={{'--mask': 'url("' + qrDataUrl() + '")'}}/>
+        {canvasEl}
     </Show>
 }
